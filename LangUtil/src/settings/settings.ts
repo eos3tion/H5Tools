19152e5185ab80c8eaa671fa2064d18909edfc2e
程方 <chengfang@tpulse-tech.com>
2019-03-27 15:52:19 +0800

    现在取消`as3`的支持，调整`ts`项目正则表达式

--- LangUtil/src/settings/settings.ts
+++ LangUtil/src/settings/settings.ts
@@ -1,54 +1,11 @@
 export default {
-    as3: {
-        ext: ".as",
-        modReg: `^\/\/@code@([^@]+)@`,
-        iHead: 1,
-        iMsg: 3,
-        iRest: 4,
-        filterStr: `(RFMessage.getMessage|RFMessage.getRealMessage|UILocator.showClientCode|UILocator.showServerCode)[(]("|')((?![$]_).*?)\\2(.*?)[)]`,
-        templateHandler: (msg: string, node: RepItem) => {
-            let head = node.head;
-            if (/RFMessage.getMessage|RFMessage.getRealMessage/.test(node.head)) {
-                head = `RFMessage.getMessage`;
-            }
-            return `${head}(${msg})`;
-        },
-        constCfg: {
-            file: `MsgCodeConst.as`,
-            head: [
-                `package`,
-                `{`,
-                `\tpublic class MsgCodeConst`,
-                `\t{`
-            ],
-            main: [
-                `\t\t/**`,
-                `\t\t * {msg}`,
-                `\t\t */`,
-                `\t\tpublic static const {code} = {code}`
-            ],
-            tail: [
-                `\t}`,
-                `}`
-            ],
-            getKey(code) {
-                return `MsgCodeConst.{0}`.substitute(this.getCode(code));
-            },
-            getCode(code) {
-                if (code != +code) {//有字符串，替换掉非法字符
-                    code = code.replace(/[^a-zA-Z$_0-9]/g, "_");
-                }
-                return `Code_{0}`.substitute(code);
-            }
-        }
-    },
     ts: {
         ext: ".ts",
         modReg: `^\/\/@code@([^@]+)@`,
         iHead: 1,
         iMsg: 2,
         iRest: 3,
-        filterStr: "(LangUtil.getMsg|CoreFunction.showClientTips|CoreFunction.showServerTips)[(]`((?![$]_).*?)`(.*?)[)]",
+        filterStr: "(LangUtil[.]getMsg|TipUtil[.]client|TipUtil[.]server)[(]`((?![$]_).*?)`(.*?)[)]",
         templateHandler: (msg: string, node: RepItem) => {
             return `${node.head}(${msg})`;
         },
