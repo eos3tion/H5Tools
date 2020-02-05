65a4011d7302321308043fdf61990034f6914182
程方 <eos_3tion@163.com>
2020-02-05 22:08:16 +0800

    调整写法，避免exlectron4+版本不允许node脚本直接执行

--- app/main.js
+++ app/main.js
@@ -1,42 +1,49 @@
 const electron = require('electron');
-const {app,Menu,BrowserWindow} = electron;
+const { app, Menu, BrowserWindow } = electron;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 let homePage = "http://127.0.0.1:12345/index.html";
-var template=[{
-    label: '视图',
-    submenu: [
-      {
-        label: '刷新',
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.reload();
-        }
-      },
-      {
-        label: '打开开发者工具',
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.toggleDevTools();
-        }
-      },
-	  {
-        label: '返回首页',
-        click: function(item, focusedWindow) {
-          if (focusedWindow)
-            focusedWindow.loadURL(homePage);
-        }
+var template = [{
+  label: '视图',
+  submenu: [
+    {
+      label: '刷新',
+      click: function (item, focusedWindow) {
+        if (focusedWindow)
+          focusedWindow.reload();
       }
-    ]
-  }];
+    },
+    {
+      label: '打开开发者工具',
+      click: function (item, focusedWindow) {
+        if (focusedWindow)
+          focusedWindow.toggleDevTools();
+      }
+    },
+    {
+      label: '返回首页',
+      click: function (item, focusedWindow) {
+        if (focusedWindow)
+          focusedWindow.loadURL(homePage);
+      }
+    }
+  ]
+}];
 var menu = Menu.buildFromTemplate(template);
 Menu.setApplicationMenu(menu);
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true
+    }
+  });
 
   // and load the index.html of the app.
   // http://h5tools.client.jy/index.html
